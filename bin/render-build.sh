2003cfe0ea89fe36f4cac3a083942acc97eb901a
tya-suke <m.keisuke22@gmail.com>
2022-12-10 18:31:00 +0900

    コード変更

---
+++ bin/render-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
